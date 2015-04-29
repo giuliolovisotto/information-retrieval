d23a32515f06bc029c514caafd32af6dc5e28dc5
giulio <giuliolovisotto@gmail.com>
2015-04-29 12:49:53 +0200

    Cleanup and move stuff and lab4

--- lab4/main.py
+++ lab4/main.py
@@ -5,18 +5,100 @@ import sys
 from joblib import cpu_count, Parallel, delayed
 
 
-def p(fm, q_id, pqw, pwords, dls, pj, res, k1, k2, b, avdl, N):
+def get_R(query_id):
+    a = np.loadtxt("data/qrels-treceval.txt", dtype='str')[:, 0].astype(int)
+    R = np.count_nonzero(a == int(query_id))
+    return R
+
+
+def get_r_i_pseudo(mat_freq, rel_ids, query_term):
+    mr = np.copy(mat_freq[rel_ids, :])
+    r_i = np.count_nonzero(mr[:, int(query_term)])
+    return r_i
+
+
+def get_r_i(mat_freq, query_id, query_term):
+    a = np.loadtxt("data/qrels-treceval.txt", dtype='str')
+    b = a[:, 0].astype(int)
+    a = a[b == int(query_id)]
+    a = a[:, 2].astype(int)
+    a -= 1
+    mr = np.copy(mat_freq[a, :])
+    r_i = np.count_nonzero(mr[:, int(query_term)])
+    return r_i
+
+
+def p_pseudo(fm, q_id, pqw, pwords, dls, pj, res, k1, k2, b, avdl, N):
     print pj
     actual_qw = []
     for qw in pqw:
         if qw in pwords:
             actual_qw.append(qw)
+
     for i, row in enumerate(fm):
         score = 0
-        for qw in actual_qw:
+        for jj, qw in enumerate(actual_qw):
             index_of_qw = pwords[qw]
             n_i = np.count_nonzero(fm[:, index_of_qw])
-            idf = np.log((N - n_i + 0.5)/(n_i + 0.5))
+            idf = np.log((N - n_i + 0.5) * 0.5/(n_i + 0.5) * 0.5)
+            dl = dls[i]
+            K = k1*((1-b) + b*(dl/avdl))
+            tf1 = (k1 + 1)*fm[i, index_of_qw]/(K + fm[i, index_of_qw])
+            tf2 = (k2 + 1)*1/(k2 + 1)
+            score += idf * tf1 * tf2
+            res[pj, i, :] = np.array([i, score])
+
+    ranking = res[pj, :, :]
+    R = 10
+    indx = np.argsort(ranking[:, 1])[::-1][0:R]
+    relevants = ranking[indx, :]
+    rel_ids = relevants[:, 0].astype(int)
+
+    r_is = []
+    for qw in actual_qw:
+        index_of_qw = pwords[qw]
+        r_is.append(get_r_i_pseudo(fm, rel_ids, index_of_qw))
+
+    r_is = np.array(r_is)
+
+    for i, row in enumerate(fm):
+        score = 0
+        for jj, qw in enumerate(actual_qw):
+            index_of_qw = pwords[qw]
+            r_i = r_is[jj]
+            n_i = np.count_nonzero(fm[:, index_of_qw])
+            idf = np.log((N - n_i - R + r_i + 0.5)*(r_i + 0.5)/(n_i - r_i + 0.5)*(R - r_i + 0.5))
+            dl = dls[i]
+            K = k1*((1-b) + b*(dl/avdl))
+            tf1 = (k1 + 1)*fm[i, index_of_qw]/(K + fm[i, index_of_qw])
+            tf2 = (k2 + 1)*1/(k2 + 1)
+            score += idf * tf1 * tf2
+            res[pj, i, :] = np.array([i, score])
+
+
+def p_esplicito(fm, q_id, pqw, pwords, dls, pj, res, k1, k2, b, avdl, N):
+    print pj
+    actual_qw = []
+    r_is = []
+    for qw in pqw:
+        if qw in pwords:
+            actual_qw.append(qw)
+            index_of_qw = pwords[qw]
+            r_is.append(get_r_i(fm, q_id, index_of_qw))
+
+    R = get_R(q_id)
+
+    r_is = np.array(r_is)
+
+    for i, row in enumerate(fm):
+        score = 0
+        for jj, qw in enumerate(actual_qw):
+            index_of_qw = pwords[qw]
+            # print q_id, index_of_qw
+            r_i = r_is[jj]
+            # print "termine: %s, compare in %s" % (qw, r_i)
+            n_i = np.count_nonzero(fm[:, index_of_qw])
+            idf = np.log((N - n_i - R + r_i + 0.5)*(r_i + 0.5)/(n_i - r_i + 0.5)*(R - r_i + 0.5))
             dl = dls[i]
             K = k1*((1-b) + b*(dl/avdl))
 
@@ -78,14 +160,17 @@ def retrieve():
             queries[q_id].append(w)
     #print queries
     N = freq_mat.shape[0]
-    k1 = 1.2
+    k1 = 0.01
     k2 = 1.2
     b = 0.0  # lascio basso perche normalizzare sulla lunghezza e' un po inutile nel nostro caso (abbiamo solo le kw)
     avdl = np.mean(docs_length)
 
     results = np.memmap("tmp", shape=(len(queries.keys()), N, 2), mode='w+', dtype='float')
 
-    Parallel(n_jobs=cpu_count())(delayed(p)(
+    #for pj, (q, lst) in enumerate(queries.iteritems()):
+    #    p_pseudo(freq_mat, q, lst, words, docs_length, pj, results, k1, k2, b, avdl, N)
+
+    Parallel(n_jobs=cpu_count())(delayed(p_pseudo)(
         freq_mat, q, lst, words, docs_length, pj, results, k1, k2, b, avdl, N
     ) for pj, (q, lst) in enumerate(queries.iteritems()))
 
