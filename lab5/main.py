bd5cc45250a29a841095ff50d1afe9ceb5f04f8d
giulio <giuliolovisotto@gmail.com>
2015-05-06 11:57:51 +0200

    Stuff for lab5

---
+++ lab5/main.py
@@ -0,0 +1,162 @@
+__author__ = 'giulio'
+
+import numpy as np
+import sys
+import networkx as nx
+# from joblib import cpu_count, Parallel, delayed
+
+
+def pagerank(edgefile):
+    G = nx.read_edgelist("../data/citation.txt", delimiter="\t", create_using=nx.DiGraph())
+    myalpha = .85
+    pranks = nx.pagerank(G, alpha=myalpha)
+    mat = np.zeros(shape=(len(pranks.keys()), 2))
+    for k, v in pranks.iteritems():
+        mat[int(k)-1, 0] = int(k)-1
+        mat[int(k)-1, 1] = v
+    np.savetxt("pagerank.txt", mat, fmt="%d %.16f")
+
+
+def get_R(query_id):
+    a = np.loadtxt("../data/qrels-treceval.txt", dtype='str')[:, 0].astype(int)
+    R = np.count_nonzero(a == int(query_id))
+    return R
+
+
+def get_r_i_pseudo(mat_freq, rel_ids, query_term):
+    mr = np.copy(mat_freq[rel_ids, :])
+    r_i = np.count_nonzero(mr[:, int(query_term)])
+    return r_i
+
+
+def get_r_i(mat_freq, query_id, query_term):
+    a = np.loadtxt("../data/qrels-treceval.txt", dtype='str')
+    b = a[:, 0].astype(int)
+    a = a[b == int(query_id)]
+    a = a[:, 2].astype(int)
+    a -= 1
+    mr = np.copy(mat_freq[a, :])
+    r_i = np.count_nonzero(mr[:, int(query_term)])
+    return r_i
+
+
+def p_original(fm, q_id, pqw, pwords, dls, pj, res, k1, k2, b, avdl, N):
+    actual_qw = []
+    indexes_of_qws = []
+    for qw in pqw:
+        if qw in pwords:
+            actual_qw.append(qw)
+            indexes_of_qws.append(pwords[qw])
+
+    indexes_of_qws = np.array(indexes_of_qws)
+    tmp = np.arange(0, fm.shape[1])
+    indexes_of_qws = np.in1d(tmp, indexes_of_qws)
+    red_fm = fm[:, indexes_of_qws]
+    idfs = np.ones(shape=(red_fm.shape[0], red_fm.shape[1]))
+    tmp2 = np.copy(red_fm)
+    tmp2[tmp2 != 0] = 1
+    nis = tmp2.sum(axis=0)
+    Ns = np.ones(red_fm.shape[1])*N
+    idfs = np.log((Ns - nis + 0.5)/(nis + 0.5))
+    Ks = k1*((1-b) + b*(dls/avdl))
+    tf1s = red_fm*(k1 + 1)/(np.tile(Ks, (red_fm.shape[1], 1)).T + red_fm)
+    tf2s = np.ones(red_fm.shape)
+    ress = np.multiply(idfs, tf1s)
+    ress = ress.sum(axis=1)
+    idss = np.arange(0, red_fm.shape[0])
+    # res[pj, :, :] = np.vstack((idss, ress)).T
+    alpha = 0.9
+
+    pageranks = np.loadtxt("pagerank.txt")[:, 1]
+
+    pageranks = (pageranks - pageranks.min()) / (pageranks.max() - pageranks.min())
+
+    ress = (ress - ress.min()) / (ress.max() - ress.min())
+
+    ress = alpha * ress + (1 - alpha) * pageranks
+
+    #for i, row in enumerate(ress):
+        #ress[i] = alpha * ress[i] + (1 - alpha) * pageranks[i]
+
+    res[pj, :, :] = np.vstack((idss, ress)).T
+
+
+def indexing():
+    freq_docid_words = np.loadtxt("../data/freq.docid.stem.txt", dtype='str')
+
+    freq_docid_words[:, 1] = (freq_docid_words[:, 1].astype(int) - 1).astype(str)
+
+    words = np.unique(freq_docid_words[:,2])
+    words_dict = {}
+    for i, w in enumerate(words):
+        words_dict[w] = i
+
+    n_words = len(words)
+    print "%s distinct words" % n_words
+
+    f = open('../data/docid.only-keywords.txt')
+
+    n_docs = len(f.readlines())
+    print "%s documents" % n_docs
+
+    f.close()
+
+    terms_mat = np.zeros(shape=(n_docs, n_words))
+
+    for r in freq_docid_words:
+        freq, doc_id, word = int(r[0]), int(r[1]), r[2]
+        word_index = np.where(words==word)[0]
+        terms_mat[doc_id, word_index] = freq
+
+    terms_mat = terms_mat.astype(float)
+
+    docs_length = terms_mat.sum(axis=1)
+
+    terms_mat /= terms_mat.sum(axis=1)[:, None]
+
+    return terms_mat, docs_length, words_dict
+
+
+def retrieve():
+    freq_mat, docs_length, words = indexing()
+
+    query_stem = np.loadtxt("../data/query-stem.txt", dtype='str', delimiter="\t ")
+
+    queries = {}
+
+    for i, row in enumerate(query_stem):
+        q_id = row[0]
+        w = row[1]
+        if q_id not in queries:
+            queries[q_id] = [w]
+        else:
+            queries[q_id].append(w)
+
+    N = freq_mat.shape[0]
+    k1 = 0.01
+    k2 = 1.2
+    b = 0.0  # lascio basso perche normalizzare sulla lunghezza e' un po inutile nel nostro caso (abbiamo solo le kw)
+    avdl = np.mean(docs_length)
+
+    results = np.memmap("tmp", shape=(len(queries.keys()), N, 2), mode='w+', dtype='float')
+
+    for pj, (q, lst) in enumerate(queries.iteritems()):
+        print q
+        p_original(freq_mat, q, lst, words, docs_length, pj, results, k1, k2, b, avdl, N)
+
+    # considera e retrieva solo i documenti con uno score almeno threshold
+    # aumento threshold, diminuisce percentuale di documenti rilevanti retrieved
+    threshold = 0.0
+
+    f = open('results.txt', 'w')
+    for j, query in enumerate(results):
+        indx = np.argsort(results[j, :, 1])[::-1][0:1000]
+        stuff_toprint = results[j, indx]
+        stuff_toprint = stuff_toprint[stuff_toprint[:, 1] > threshold]
+        for i, row in enumerate(stuff_toprint):
+            f.write("%s Q0 %s %s %s G12R9PR\n" % (queries.keys()[j], int(row[0]+1), i+1, row[1]))
+    f.close()
+
+if __name__ == "__main__":
+
+    retrieve()
