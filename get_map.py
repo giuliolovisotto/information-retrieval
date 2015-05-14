0d5da05f6812fcec8239052951d14dfa7ce83131
giulio <giuliolovisotto@gmail.com>
2015-05-14 11:04:37 +0200

    lab7 prima consegna

---
+++ get_map.py
@@ -0,0 +1,10 @@
+import subprocess
+import re
+
+def evaluate_map(resultfile):
+    cmd = [ './trec_eval.9.0/trec_eval', 'data/qrels-treceval.txt', resultfile ]
+    output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+    i = re.search("map", output).start()
+    endl = re.search("\n", output[i:]).start()
+    maprow = output[i:i+endl]
+    return float(maprow.split('\t')[2])
