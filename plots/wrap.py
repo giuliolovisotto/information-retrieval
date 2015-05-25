8b78d4519ae079751a751db0df9a98904faf056a
giulio <giuliolovisotto@gmail.com>
2015-05-25 23:55:49 +0200

    Lab7 plotting.

--- plots/wrap.py
+++ plots/wrap.py
@@ -4,25 +4,30 @@ import subprocess
 from get_map import *
 import numpy as np
 
-dim = 21
+dim = 10
 
-params = np.zeros(shape=(dim, 4))
+params = np.zeros(shape=(dim, 7))
 
 maps = np.zeros(dim)
 
-params[:, 0] = 0.1  # k1
-params[:, 1] = 0.0  # b
+params[:, 0] = 0.02  # k1
+params[:, 1] = 0.02  # b
 params[:, 2] = 1.0  # k2
-params[:, 3] = np.linspace(0.0, 1.0, dim)  # alpha
+params[:, 3] = np.linspace(10, 100, dim)  # Nd
+params[:, 4] = 1.0  # alpha
+params[:, 5] = 0.1  # beta
+params[:, 6] = 0.1  # gamma
+
 
 
 for i in range(params.shape[0]):
     subprocess.call([
-        'python', '../lab5/main.py', str(params[i, 0]), str(params[i, 1]), str(params[i, 2]), str(params[i, 3])
+        'python', '../lab7/main.py', str(params[i, 0]), str(params[i, 1]), str(params[i, 2]), str(params[i, 3]),
+        str(params[i, 4]), str(params[i, 5]), str(params[i, 6]),
     ])
     maps[i] = evaluate_map("results.txt")
     print params[i, 3], maps[i]
 
 # header = b, map
 
-np.savetxt("lab5_plots_alpha.txt", np.vstack((params[:, 3], maps)).T, delimiter=',', header="alpha,map", comments='')
+np.savetxt("lab7_plots_N.txt", np.vstack((params[:, 3], maps)).T, delimiter=',', header="alpha,map", comments='')
